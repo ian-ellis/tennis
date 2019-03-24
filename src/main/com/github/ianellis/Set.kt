b4513605342980040fda958f1ef1f653e110c08e
ian-ellis <ian.ellis@qantasloyalty.com>
2019-03-24 23:32:25 +1100

    Adding Set Implementation
    Shared a lot of code with the StandardGame and TieBreak classes here but there are more subtle differences. I thought about storing the game instances that each player had won as this could be a good source of information, however at this point it was not needed to so I decided to use an Int to determine the number of games each player had won.
    
    After that it was simply a case of updating the way we managed the state and changing the logic for whether a set had been won, followed by which game should be pulled next.
    
    I considered providing factories to create new instances of the Game interfaces to help with testing but without a mocking framework that could verify calls to the mocks I decided it was not worth it (as i would have had to roll my own)

--- src/main/com/github/ianellis/Set.kt
+++ src/main/com/github/ianellis/Set.kt
@@ -5,20 +5,79 @@ class Set(
     private val player2: Player
 ) : Game {
 
-    override fun start() {
-
+    companion object {
+        private const val MIN_GAMES_TO_WIN = 6
+        private const val MIN_WINNING_MARGIN = 2
     }
 
-    override fun pointWonBy(player: Player) {
+    private var currentGame: Game = StandardGame(player1, player2)
 
+    private var player1Games: Int = 0
+    private var player2Games: Int = 0
+
+    private var state: GameState = GameState.NotStarted
+
+    override fun start() {
+        if (state == GameState.NotStarted) {
+            state = GameState.Started(score())
+        }
+    }
+
+    override fun state(): GameState = state
+
+    override fun pointWonBy(player: Player) {
+        if (state() !is GameState.Complete) {
+            currentGame.pointWonBy(player)
+            val currentGameState = currentGame.state()
+            if (currentGameState is GameState.Complete) {
+                currentGame.wonBy(currentGameState.winner)
+            }
+            updateState()
+        }
     }
 
     override fun score(): String {
-        return ""
+        return state.let {
+            when (it) {
+                is GameState.Started -> it.score
+                is GameState.NotStarted -> "0-0"
+                is GameState.Complete -> "$player1Games-$player2Games"
+            }
+        }
     }
 
-    override fun state(): GameState {
-        return GameState.NotStarted
+    private fun Game.wonBy(winner: Player) {
+        when (winner) {
+            player1 -> player1Games ++
+            player2 -> player2Games ++
+        }
+        currentGame = nextGame()
+        currentGame.start()
+        updateState()
+    }
+
+    private fun updateState() {
+
+        state = winner()?.let {
+            GameState.Complete(it)
+        } ?: kotlin.run {
+            GameState.Started("$player1Games-$player2Games, ${currentGame.score()}")
+        }
+    }
+
+    private fun nextGame(): Game {
+        return when {
+            player1Games == MIN_GAMES_TO_WIN && player2Games == MIN_GAMES_TO_WIN -> TieBreak(player1, player2)
+            else -> StandardGame(player1, player2)
+        }
+    }
+
+    private fun winner(): Player? {
+        return when {
+            player1Games >= MIN_GAMES_TO_WIN && (player1Games >= player2Games + MIN_WINNING_MARGIN) -> player1
+            player2Games >= MIN_GAMES_TO_WIN && (player2Games >= player1Games + MIN_WINNING_MARGIN) -> player2
+            else -> null
+        }
     }
 
 
