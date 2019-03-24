e41dd4eef2dc651ffcbac4cef9a91458cc8cd716
ian-ellis <ian.ellis@qantasloyalty.com>
2019-03-25 09:35:53 +1100

    Adding Match Specification
    Because a match is simply one set, we can design it to simply proxy one set object. As we cannot use any mocking libs we have to write our own argument captures using an custom implementation of the Game interface and a in-scope variable

---
+++ src/main/com/github/ianellis/Match.kt
@@ -0,0 +1,17 @@
+package com.github.ianellis
+
+class Match(
+    private val player1: Player,
+    private val player2: Player,
+    private val sets: List<Set>
+) {
+
+    fun pointWonBy(player: Player) {
+
+    }
+
+    fun score(): String {
+        return ""
+    }
+}
+
---
+++ src/test/com/github/ianellis/MatchTest.kt
@@ -0,0 +1,45 @@
+package com.github.ianellis
+
+import org.junit.Test
+import kotlin.test.assertEquals
+
+class MatchTest {
+
+
+    @Test
+    fun `score() - returns score from set`() {
+        val score = "0-0, 15-0"
+        val set = gameWith(score)
+        val match = Match(set)
+        assertEquals(score, match.score())
+    }
+
+    @Test
+    fun `pointWonBy() - invokes set pointsWonByMethod on set`() {
+        var pointsWonByCaptureValue: Player? = null
+        val set = object : Game {
+            override fun start() {}
+            override fun pointWonBy(player: Player) {
+                pointsWonByCaptureValue = player
+            }
+            override fun score(): String = ""
+            override fun state() = GameState.NotStarted
+
+        }
+        val match = Match(set)
+        val player = Player("player1")
+        match.pointWonBy(player)
+
+        assertEquals(player, pointsWonByCaptureValue)
+    }
+
+    private fun gameWith(score: String): Game {
+        return object : Game {
+            override fun start() {}
+            override fun pointWonBy(player: Player) {}
+            override fun score(): String = score
+            override fun state() = GameState.NotStarted
+        }
+    }
+
+}
